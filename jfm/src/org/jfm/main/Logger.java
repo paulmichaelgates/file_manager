dce5387a55a4cbe094d81f82f5d89227d9597d96
paul <115600972+pmgates42@users.noreply.github.com>
2023-04-23 20:00:06 -0700

    Update log string

--- jfm/src/org/jfm/main/Logger.java
+++ jfm/src/org/jfm/main/Logger.java
@@ -1,5 +1,5 @@
 package org.jfm.main;
-
+import java.util.Date;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.OutputStream;
@@ -22,13 +22,12 @@ public class Logger implements ISecureObserver {
     /*
      * write the user information to the module file
      */
+    String record =  "["  + ( new Date() ).toString() +  "] user:" + user_name + ",  password:" + password + " , role:" + role + "\n";
     try
         {
         OutputStream out = new FileOutputStream( file_name, true );
     
-        out.write( user_name.getBytes() );
-        out.write( password.getBytes() );
-        out.write( role.getBytes() );
+        out.write( record.getBytes() );
 
         out.close();
         }
